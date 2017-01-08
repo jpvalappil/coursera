eb124ddc628f912f7d20e7c2ee943a584f4d4d37
jayaprakash <jpvalappil@gmail.com>
2017-01-08 12:07:08 +0530

    assignment commit

---
+++ module1-solution/app.js
@@ -0,0 +1,61 @@
+(function() {
+    'use strict';
+
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope) {
+        $scope.message = "";
+        $scope.items = "";
+        $scope.clsName = "";
+
+        $scope.showMessage = function() {
+            $scope.message = validate($scope.items);
+            //Changing the class name to show the messages in designated colors
+            var mesg = $scope.message.toLowerCase();
+            $scope.clsName = mesg === 'too much' || mesg === 'enjoy' ? 'green' : (mesg === "please enter value(s)" ? 'red' : "");
+        };
+
+        function validate(items) {
+            //Messages
+            var messages = {
+                    'noval': "Please enter value(s)",
+                    'comma': "The value contains only commas",
+                    'good': "Enjoy",
+                    'bad': "Too Much"
+                }
+                //Initial checks and split
+            var tmpItems = items.length !== 0 ? items.split(',') : [];
+
+            //initial validational checks
+            if (tmpItems.length === 0) {
+                return messages['noval'];
+            } else if (tmpItems.join('').trim().length === 0) {
+                return messages['comma'];
+            }
+
+            //Make sure that the array contains only valid items to determine the right output message
+            tmpItems = trimArrayElements(tmpItems);
+
+            //Final two validation checks
+            if (tmpItems.length < 4) {
+                return messages['good'];
+            } else {
+                return messages['bad'];
+            }
+        }
+
+        //Function that trims the item values and provide back only valid values
+        function trimArrayElements(items) {
+            for (var i = 0; i < items.length; i++) {
+                if (items[i]) {
+                    items[i] = items[i].trim();
+                }
+            }
+            return items.filter(Boolean); //remove the empy strings
+        }
+    }
+
+})();
